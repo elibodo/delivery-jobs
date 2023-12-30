6e46ab6daa983624d112928d938febf9f82e4fdc
Eli Bodovinitz <ebodovinitz@gmail.com>
2023-12-30 17:32:39 -0500

    added items to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex_center flex-col space-y-4">
       <h1 className="head_text text-center">
         Delivery Jobs
         <br />
-        <span className="text-center">
-          A new tool to help you find a driving job
+        <br />
+        <span className="description text-center">
+          The best tool to help drivers find jobs and for employers to find
+          drivers
         </span>
+        <br />
+        <button className="button_viewJobs">View Jobs</button>
       </h1>
     </section>
   );
