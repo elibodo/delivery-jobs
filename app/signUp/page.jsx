5b23b202623101e4bd25b444496011eb1d40f909
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-01-09 21:43:17 -0500

    styled sign up page

--- app/signUp/page.jsx
+++ app/signUp/page.jsx
@@ -1,61 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import "@styles/globals.css";
 
 const signUp = () => {
   return (
     <section className="flex flex-col justify-center content-center items-center bg-gray-200 border-black border-2 p-4 rounded-xl">
       <h1 className="text-2xl font-bold mb-4">Create an Account</h1>
-      <div className="flex flex-col border border-black m-3 p-2 rounded-xl bg-white">
-        <label>
-          What is your first name?
-          <input
-            type="text"
-            required
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label className=" justify-between">
-          What is your last name?
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label>
-          What is your companies name?
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label>
-          What is your email address?
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label>
-          Confirm your email address
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label>
-          Enter a password
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
-        <label>
-          Confirm your password
-          <input
-            type="text"
-            className="p-1 m-2 border border-black rounded-lg bg-gray-100"
-          ></input>
-        </label>
+      <div className="flex flex-col justify-between border border-black m-3 p-2 rounded-xl bg-white">
+        <div className="label_input_text">
+          <label>What is your first name?</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>What is your last name?</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>What is your companies name?</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>What is your email address?</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>Please confirm your email address</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>Enter a password</label>
+          <input type="text" required className="input_style"></input>
+        </div>
+        <div className="label_input_text">
+          <label>Confirm your password</label>
+          <input type="text" required className="input_style"></input>
+        </div>
       </div>
       <div>
         <Link href={"/"} className="black_button">
