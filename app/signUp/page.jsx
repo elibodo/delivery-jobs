82eeca7fff4b217991800c40a7c686a9aeb0de2f
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-01-10 19:46:20 -0500

    finished styling the create page and created new pages for the application

--- app/signUp/page.jsx
+++ app/signUp/page.jsx
@@ -8,31 +8,42 @@ const signUp = () => {
       <h1 className="text-2xl font-bold mb-4">Create an Account</h1>
       <div className="flex flex-col justify-between border border-black m-3 p-2 rounded-xl bg-white">
         <div className="label_input_text">
-          <label>What is your first name?</label>
+          <label className="mr-5">Are You a Job Seeker or Employer?</label>
+          <div className="flex flex-col m-auto">
+            <label>
+              Job Seeker <input type="radio" name="radiobtn"></input>
+            </label>
+            <label>
+              Employer <input type="radio" name="radiobtn"></input>
+            </label>
+          </div>
+        </div>
+        <div className="label_input_text">
+          <label className="mr-5">What is your first name?</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>What is your last name?</label>
+          <label className="mr-5">What is your last name?</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>What is your companies name?</label>
+          <label className="mr-5">What is your companies name?</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>What is your email address?</label>
+          <label className="mr-5">What is your email address?</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>Please confirm your email address</label>
+          <label className="mr-5">Please confirm your email address</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>Enter a password</label>
+          <label className="mr-5">Enter a password</label>
           <input type="text" required className="input_style"></input>
         </div>
         <div className="label_input_text">
-          <label>Confirm your password</label>
+          <label className="mr-5">Confirm your password</label>
           <input type="text" required className="input_style"></input>
         </div>
       </div>
