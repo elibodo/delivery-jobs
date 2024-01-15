6f70d9894056e2b93d9630a2f784879d4aa8467b
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-01-15 15:11:42 -0500

    Added forgot password page

--- components/ForgotPassword.jsx
+++ components/ForgotPassword.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <section className="flex flex-col justify-center content-center items-center bg-gray-100 border-black border-2 p-4 rounded-xl">
+      <h1 className="text-2xl font-bold mb-4">Forgot Password</h1>
+      <div className="">
+        <label className="mr-5">Enter your email</label>
+        <input type="email" required className="input_style"></input>
+      </div>
+      <Link href={"/resetPassword"} className="black_button mt-4">
+        Send code to email
+      </Link>
+    </section>
+  );
 };
 
 export default ForgotPassword;
