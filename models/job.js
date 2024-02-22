7a49eb69bb2f415e2e226c23010a6a8f64d2551e
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-02-22 00:39:58 -0500

    schema is working and can add to it

--- models/job.js
+++ models/job.js
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 
 const JobSchema = new Schema({
-  employer: {
+  creator: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
@@ -9,13 +9,17 @@ const JobSchema = new Schema({
     type: String,
     required: [true, "Title is required."],
   },
-  location: {
-    type: String,
-    required: [true, "Location is required."],
-  },
   description: {
     type: String,
     required: [true, "Description is required."],
+  },
+  dispatchlocation: {
+    type: String,
+    required: [true, "Dispatch Location is required."],
+  },
+  deliverylocation: {
+    type: String,
+    required: [true, "Delivery Location is required."],
   },
 });
 
