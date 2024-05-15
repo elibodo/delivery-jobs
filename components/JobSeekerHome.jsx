e43a6b9b8a96eb95b9e8646e66b8890894d9f28c
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-05-15 18:06:11 -0400

    data is shown on js account

--- components/JobSeekerHome.jsx
+++ components/JobSeekerHome.jsx
@@ -1,13 +1,16 @@
 "use client";
 
 import React from "react";
-import { useSession } from "next-auth/react";
 
-const JobSeekerHome = (accountDetails) => {
-  const { data: session } = useSession();
-  console.log("asdf" + JSON.stringify(accountDetails));
-
-  return <div>{accountDetails.twikCard}</div>;
+const JobSeekerHome = ({ account }) => {
+  return (
+    <section>
+      <p>{account.email}</p>
+      <div>
+        <p>Hello</p>
+      </div>
+    </section>
+  );
 };
 
 export default JobSeekerHome;
