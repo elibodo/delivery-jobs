91c8e277e95bc12a0016f655bec11c998be166f1
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-05-16 18:42:08 -0400

    no duplicates in applicants

--- app/api/job/apply/route.js
+++ app/api/job/apply/route.js
@@ -4,21 +4,20 @@ import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   await connectToDB();
-
   if (req.method === "POST") {
-    const { postId, applicant } = await req.json();
+    const { postId, applicants } = await req.json();
     try {
       const job = await Job.findById(postId);
       if (!job) {
         return res.status(404).json({ message: "Job not found." });
       }
-      job.applicants.push({ applicant });
+      await job.applicants.push(applicants);
       await job.save();
-      return NextResponse.json({ message: "failed" });
+      return NextResponse.json({ message: "added user to applicants" });
     } catch (error) {
       return NextResponse.json({ message: "failed" }, { status: 500 });
     }
   } else {
-    return NextResponse.json({ message: "failsed" }, { status: 500 });
+    return NextResponse.json({ message: "failed" }, { status: 500 });
   }
 }
