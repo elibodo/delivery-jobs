08562cc9bc15fb8f793f34bd16cfb6dd88d0fa70
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-05-16 21:54:46 -0400

    asdf

--- app/employerAccount/employerMessaging/page.jsx
+++ app/employerAccount/employerMessaging/page.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 const EmployerMessaging = () => {
-  const companyName = "White Sands Delivery Inc";
-
   return (
     <div>
       <div className="flex flex-row items-center justify-between p-2 mx-3 border-b-2 border-gray-500">
         <h1 className="font-bold text-2xl">Messaging</h1>
-        <p className="">{companyName}</p>
       </div>
     </div>
   );
