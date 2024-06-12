905ae0c1ff4cffff78855d84ea1f5b611dda188c
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-06-12 15:25:11 -0400

    updated files

--- app/employerAccount/myJobs/page.jsx
+++ app/employerAccount/myJobs/page.jsx
@@ -4,29 +4,10 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import MyJobCard from "@components/MyJobCard";
-
 import React from "react";
 
 const MyJobs = () => {
-  const companyName = "White Sands Delivery Inc";
-
-  const { data: session } = useSession();
-  const [myPosts, setMyPosts] = useState([]);
-
-  const handleEdit = () => {};
-
-  const handleDelete = async () => {};
-
-  return (
-    <MyJobCard
-      name=""
-      desc="asdfasdf"
-      data={[myPosts]}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
-  );
+  return <div>asdf</div>;
 };
 
 export default MyJobs;
