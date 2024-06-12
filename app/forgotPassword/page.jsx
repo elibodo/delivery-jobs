b6aafe44bfad66c4fa7366584d339d17e26dbd2c
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-06-12 17:17:17 -0400

    deleted files

--- app/forgotPassword/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ForgotPassword from "@components/ForgotPassword";
-
-const forgotPassword = () => {
-  return <ForgotPassword />;
-};
-
-export default forgotPassword;
