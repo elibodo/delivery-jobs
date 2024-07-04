c3513d4bd27dc39445640ed68babf4352f2e9936
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-07-04 14:30:22 -0400

    terms and conditions

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer_text">
+    <footer className="footer_text flex flex-col gap-2">
       &copy; {new Date().getFullYear().toString()} Delivery Jobs
+      <a href="/termsAndConditions">Terms and Conditions</a>
     </footer>
   );
 };
