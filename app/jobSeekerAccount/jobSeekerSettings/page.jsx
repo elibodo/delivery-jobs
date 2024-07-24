3474496f4ea1c2b61f073b56fe27325328cbc4f1
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-07-24 15:52:31 -0400

    job seeker settings page functioning

--- app/jobSeekerAccount/jobSeekerSettings/page.jsx
+++ app/jobSeekerAccount/jobSeekerSettings/page.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+"use client";
 
-const JobSeekerSettings = () => {
+import React from "react";
+import JobSeekerSettings from "@components/JobSeekerSettings";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+const JSSettings = ({ accountData }) => {
+  return (
+    <div>
+      {accountData.map((account) => (
+        <JobSeekerSettings key={account._id} account={account} />
+      ))}
+    </div>
+  );
+};
+
+const SeekerSettings = () => {
+  const { data: session } = useSession();
+
+  const [accountInfo, setAccountInfo] = useState([]);
+  useEffect(() => {
+    const fetchAccount = async () => {
+      const response = await fetch(
+        `/api/account/${session?.user?.email}/jobseeker`,
+        { method: "GET" }
+      );
+      const data = await response.json();
+      setAccountInfo(data);
+    };
+    if (session?.user.email) fetchAccount();
+  }, []);
   return (
     <div>
       <div className="flex flex-row items-center justify-between p-2 mx-3 border-b-2 border-gray-500">
         <h1 className="font-bold text-2xl">Settings</h1>
       </div>
-      <p className="description flex justify-center">Coming soon!</p>
+      <JSSettings accountData={accountInfo} />
     </div>
   );
 };
 
-export default JobSeekerSettings;
+export default SeekerSettings;
