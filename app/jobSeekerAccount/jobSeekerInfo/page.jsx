b4ea795a2bc0fe052f2fe3fbc4c71e8bd3281973
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-07-26 16:46:53 -0400

    job seeker update information page

--- app/jobSeekerAccount/jobSeekerInfo/page.jsx
+++ app/jobSeekerAccount/jobSeekerInfo/page.jsx
@@ -1,12 +1,41 @@
+"use client";
+
 import React from "react";
+import JobSeekerInformation from "@components/JobSeekerInformation";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+
+const JSInformation = ({ accountData }) => {
+  return (
+    <div>
+      {accountData.map((account) => (
+        <JobSeekerInformation key={account._id} account={account} />
+      ))}
+    </div>
+  );
+};
 
 const JobSeekerInfo = () => {
+  const { data: session } = useSession();
+
+  const [accountInfo, setAccountInfo] = useState([]);
+  useEffect(() => {
+    const fetchAccount = async () => {
+      const response = await fetch(
+        `/api/account/${session?.user?.email}/jobseeker`,
+        { method: "GET" }
+      );
+      const data = await response.json();
+      setAccountInfo(data);
+    };
+    if (session?.user.email) fetchAccount();
+  }, []);
   return (
     <div>
       <div className="flex flex-row items-center justify-between p-2 mx-3 border-b-2 border-gray-500">
-        <h1 className="font-bold text-2xl">Profile Information</h1>
+        <h1 className="font-bold text-2xl">Resume Information</h1>
       </div>
-      <p className="description flex justify-center">Coming soon!</p>
+      <JSInformation accountData={accountInfo} />
     </div>
   );
 };
