ed5a7dc3ba86e8e5b9354198bda3f7710da606d0
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-08-02 20:12:18 -0400

    made mobile view for a log in, password reset, password request, and the beginning of creating an account

--- app/passwordReset/[token]/page.jsx
+++ app/passwordReset/[token]/page.jsx
@@ -25,7 +25,7 @@ const PasswordReset = (params) => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col justify-center content-center items-center bg-gray-50 border-black border-2 p-4 rounded-xl"
+      className="flex flex-col justify-center content-center items-center bg-gray-100  border-1 p-4 rounded-xl mt-4"
     >
       <h1 className="text-2xl font-bold p-5">Reset your password</h1>
       <div className="flex flex-row">
@@ -35,7 +35,7 @@ const PasswordReset = (params) => {
             type="password"
             placeholder="Enter your new password"
             required
-            className="p-2 m-2 border border-black rounded-lg bg-gray-100"
+            className="form_input p-2 m-2"
           />
           <button className="black_button m-2">Save new password</button>
           {message && <p>{message}</p>}
