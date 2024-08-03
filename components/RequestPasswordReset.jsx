a954a8e9afb0288a2b9109f4abbfc24b32e60a16
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-08-02 20:12:18 -0400

    made mobile view for a log in, password reset, password request, and the beginning of creating an account

--- components/RequestPasswordReset.jsx
+++ components/RequestPasswordReset.jsx
@@ -27,7 +27,7 @@ const RequestPasswordReset = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col justify-center content-center items-center bg-gray-50 border-black border-2 p-4 rounded-xl"
+      className="flex flex-col justify-center content-center items-center bg-gray-100  border-1 p-4 rounded-xl mt-4"
     >
       <h1 className="text-2xl font-bold p-5">Request Password Reset</h1>
       <div className="flex flex-row">
@@ -36,7 +36,7 @@ const RequestPasswordReset = () => {
             onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Email"
-            className="p-2 m-2 border border-black rounded-lg bg-gray-100"
+            className="form_input p-2 m-2"
           />
           <button className="black_button m-2">Send email</button>
           {message && <p>{message}</p>}
