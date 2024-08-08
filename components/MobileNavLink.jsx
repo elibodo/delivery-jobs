55f6f7fd618db96fe110723ce40e303ddeb21380
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-08-07 20:48:52 -0400

    created the mobile navigation links for both profiles

---
+++ components/MobileNavLink.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MobileNavLink = ({ href, children }) => {
+  const pathname = usePathname();
+  const active = href === pathname;
+  return (
+    <Link
+      href={href}
+      className={`py-1 px-2 rounded block text-xs ${
+        active ? "text-black bg-gray-100" : "text-gray-500"
+      }`}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default MobileNavLink;
