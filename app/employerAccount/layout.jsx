199d2c27477815bc79445bc53473645428880208
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-08-07 20:48:52 -0400

    created the mobile navigation links for both profiles

--- app/employerAccount/layout.jsx
+++ app/employerAccount/layout.jsx
@@ -1,41 +1,133 @@
 import ProfileNavLink from "@components/ProfileNavLink";
+import MobileNavLink from "@components/MobileNavLink";
 import React from "react";
 
 const EmployerNavLayout = ({ children }) => {
   return (
-    <div className="flex justify-center w-full overflow-x-auto">
-      <aside className="flex-1 mr-5 pb-56">
+    <div className="justify-center w-full overflow-x-auto">
+      <div className="hidden md:flex justify-center w-full overflow-x-auto">
+        <aside className="flex-1 mr-5">
+          <nav>
+            <ul className="relative grid gap-3">
+              <li>
+                <ProfileNavLink href={"/employerAccount/employerAccountHome"}>
+                  Home
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/employerAccount/createJob"}>
+                  Create Job
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/employerAccount/employerMessaging"}>
+                  Messages
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/employerAccount/candidates"}>
+                  Candidates
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/employerAccount/employerSettings"}>
+                  Settings
+                </ProfileNavLink>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <div className="w-11/12 bg-gray-100 rounded">{children}</div>
+      </div>
+      <div className="md:hidden">
         <nav>
-          <ul className="grid gap-3">
+          <ul className="grid grid-flow-col justify-stretch h-[32px]">
             <li>
-              <ProfileNavLink href={"/employerAccount/employerAccountHome"}>
-                Home
-              </ProfileNavLink>
+              <MobileNavLink href={"/employerAccount/employerAccountHome"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/employerAccount/createJob"}>
-                Create Job
-              </ProfileNavLink>
+              <MobileNavLink href={"/employerAccount/createJob"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/employerAccount/employerMessaging"}>
-                Messages
-              </ProfileNavLink>
+              <MobileNavLink href={"/employerAccount/employerMessaging"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902.848.137 1.705.248 2.57.331v3.443a.75.75 0 0 0 1.28.53l3.58-3.579a.78.78 0 0 1 .527-.224 41.202 41.202 0 0 0 5.183-.5c1.437-.232 2.43-1.49 2.43-2.903V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0 0 10 2Zm0 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM8 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/employerAccount/candidates"}>
-                Candidates
-              </ProfileNavLink>
+              <MobileNavLink href={"/employerAccount/candidates"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z" />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/employerAccount/employerSettings"}>
-                Settings
-              </ProfileNavLink>
+              <MobileNavLink href={"/employerAccount/employerSettings"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a6.993 6.993 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7.047 7.047 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a6.993 6.993 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a6.993 6.993 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7.05 7.05 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54A6.992 6.992 0 0 1 7.51 3.456l.33-1.652ZM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
           </ul>
         </nav>
-      </aside>
-      <div className="w-11/12 bg-gray-100 rounded">{children}</div>
+        <div className="bg-gray-100 rounded">{children}</div>
+      </div>
     </div>
   );
 };
--- app/jobSeekerAccount/layout.jsx
+++ app/jobSeekerAccount/layout.jsx
@@ -1,41 +1,142 @@
 import ProfileNavLink from "@components/ProfileNavLink";
+import MobileNavLink from "@components/MobileNavLink";
 import React from "react";
 
 const JobSeekerNavLayout = ({ children }) => {
   return (
-    <div className="flex justify-center w-full overflow-x-auto">
-      <aside className="flex-1 mr-5">
+    <div className="justify-center w-full overflow-x-auto">
+      <div className="hidden md:flex justify-center w-full overflow-x-auto">
+        <aside className="flex-1 mr-5">
+          <nav>
+            <ul className="relative grid gap-3">
+              <li>
+                <ProfileNavLink href={"/jobSeekerAccount/jobSeekerResume"}>
+                  Resume
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/jobSeekerAccount/jobSeekerJobs"}>
+                  Jobs
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/jobSeekerAccount/jobSeekerMessaging"}>
+                  Messaging
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/jobSeekerAccount/jobSeekerInfo"}>
+                  Update Information
+                </ProfileNavLink>
+              </li>
+              <li>
+                <ProfileNavLink href={"/jobSeekerAccount/jobSeekerSettings"}>
+                  Settings
+                </ProfileNavLink>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <div className="w-11/12 bg-gray-100 rounded">{children}</div>
+      </div>
+      <div className="md:hidden">
         <nav>
-          <ul className="grid gap-3">
+          <ul className="grid grid-flow-col justify-stretch h-[32px]">
             <li>
-              <ProfileNavLink href={"/jobSeekerAccount/jobSeekerResume"}>
-                Resume
-              </ProfileNavLink>
+              <MobileNavLink href={"/jobSeekerAccount/jobSeekerResume"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/jobSeekerAccount/jobSeekerJobs"}>
-                Jobs
-              </ProfileNavLink>
+              <MobileNavLink href={"/jobSeekerAccount/jobSeekerJobs"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6 3.75A2.75 2.75 0 0 1 8.75 1h2.5A2.75 2.75 0 0 1 14 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 0 1 6 4.193V3.75Zm6.5 0v.325a41.622 41.622 0 0 0-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25ZM10 10a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1h.01a1 1 0 0 0 1-1V11a1 1 0 0 0-1-1H10Z"
+                      clipRule="evenodd"
+                    />
+                    <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 0 1-9.274 0C3.985 17.585 3 16.402 3 15.055Z" />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/jobSeekerAccount/jobSeekerMessaging"}>
-                Messaging
-              </ProfileNavLink>
+              <MobileNavLink href={"/jobSeekerAccount/jobSeekerMessaging"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902.848.137 1.705.248 2.57.331v3.443a.75.75 0 0 0 1.28.53l3.58-3.579a.78.78 0 0 1 .527-.224 41.202 41.202 0 0 0 5.183-.5c1.437-.232 2.43-1.49 2.43-2.903V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0 0 10 2Zm0 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM8 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/jobSeekerAccount/jobSeekerInfo"}>
-                Update Information
-              </ProfileNavLink>
+              <MobileNavLink href={"/jobSeekerAccount/jobSeekerInfo"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V6Zm4 1.5a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm2 3a4 4 0 0 0-3.665 2.395.75.75 0 0 0 .416 1A8.98 8.98 0 0 0 7 14.5a8.98 8.98 0 0 0 3.249-.604.75.75 0 0 0 .416-1.001A4.001 4.001 0 0 0 7 10.5Zm5-3.75a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Zm0 6.5a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Zm.75-4a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 0-1.5h-2.5Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
             <li>
-              <ProfileNavLink href={"/jobSeekerAccount/jobSeekerSettings"}>
-                Settings
-              </ProfileNavLink>
+              <MobileNavLink href={"/jobSeekerAccount/jobSeekerSettings"}>
+                <div className="flex flex-col items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a6.993 6.993 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7.047 7.047 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a6.993 6.993 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a6.993 6.993 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7.05 7.05 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54A6.992 6.992 0 0 1 7.51 3.456l.33-1.652ZM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </MobileNavLink>
             </li>
           </ul>
         </nav>
-      </aside>
-      <div className="w-11/12 bg-gray-100 rounded">{children}</div>
+        <div className="bg-gray-100 rounded">{children}</div>
+      </div>
     </div>
   );
 };
