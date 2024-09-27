0f94380bae15b5e4be16a66a3248d6fc310df6d9
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-09-26 20:47:34 -0400

    added rich text editor to create job page

--- app/contactPage/page.jsx
+++ app/contactPage/page.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const ContactPage = () => {
-  return (
-    <div className="animate-pulse rounded-lg bg-gray-200">
-      <div className="h-16">a</div>
-      <div className="h-4">a</div>
-    </div>
-  );
+  return <div>Coming soon!</div>;
 };
 
 export default ContactPage;
