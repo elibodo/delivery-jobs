04c8852d0889115476bdc351a4b0e0d2af065881
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-09-28 13:54:35 -0400

    updated pagination

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import JobCard from "./JobCard";
 import Pagination from "./Pagination";
 import SearchJobs from "./SearchJobs";
@@ -8,6 +7,8 @@ import JobLoading from "./JobLoading";
 const Feed = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(10);
+  const [postPerPage] = useState(1);
 
   const fetchPosts = async () => {
     const response = await fetch("/api/job");
@@ -15,23 +16,26 @@ const Feed = () => {
     setPosts(data);
     setLoading(false);
   };
+
   useEffect(() => {
     fetchPosts();
   }, []);
 
-  //Pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(10);
-
+  // Get current posts for pagination
   const indexOfLastPost = currentPage * postPerPage;
   const indexOfFirstPost = indexOfLastPost - postPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const getSearchResults = (results) => {
+    setPosts(results);
+    setCurrentPage(1); // Reset to page 1 when new results are fetched
+  };
+
   return (
     <section className="feed">
-      <SearchJobs getSearchResults={(results) => setPosts(results)} />
+      <SearchJobs getSearchResults={getSearchResults} />
       <div className="feed">
         <div className="mt-4 columns-1 space-y-4 py-4">
           {loading ? (
@@ -39,11 +43,9 @@ const Feed = () => {
           ) : (
             <>
               {currentPosts.length > 0 ? (
-                <>
-                  {currentPosts.map((post) => (
-                    <JobCard key={post._id} post={post} />
-                  ))}
-                </>
+                currentPosts.map((post) => (
+                  <JobCard key={post._id} post={post} />
+                ))
               ) : (
                 <div className="description text-center">
                   <p className="font-semibold">No Jobs Available</p>
@@ -60,6 +62,7 @@ const Feed = () => {
           postPerPage={postPerPage}
           totalPosts={posts.length}
           paginate={paginate}
+          currentPage={currentPage} // Pass currentPage to Pagination component
         />
       </div>
     </section>
