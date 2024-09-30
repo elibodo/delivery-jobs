3959bb91fcdff571cd44f2a4655cd174472beaa8
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-09-30 17:47:06 -0400

    changed button style, imported analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import Provider from "@components/Provider";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Delivery Jobs",
@@ -31,6 +32,7 @@ const RootLayout = ({ children }) => {
           <main className="app">
             <Header />
             {children}
+            <Analytics />
             <Footer />
           </main>
         </body>
