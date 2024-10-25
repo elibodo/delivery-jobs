7cb662baf450d3bb1e0a1f04d6e78620508592ed
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-10-25 12:15:21 -0400

    changed job seeker job page

--- components/JobSeekerJobPage.jsx
+++ components/JobSeekerJobPage.jsx
@@ -2,24 +2,24 @@ import React, { useState, useRef } from "react";
 
 const JobSeekerJobPage = ({ jobs }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const contentRef = useRef(null); // Reference to the description element
+  const contentRef = useRef(null);
 
   const toggleDescription = () => {
     setIsExpanded(!isExpanded);
   };
 
   // Safely access the payrange array
-  const payrange = jobs?.payrange || ["", "", ""]; // Default value if undefined
+  const payrange = jobs?.payrange || ["", "", ""];
+  const additionalpay = jobs?.additionalpay || ["", ""];
 
   if (jobs === "Job Has Been Removed" || !jobs) {
-    return null; // Return null if job has been removed or jobs is undefined
+    return null;
   }
 
   return (
-    // The whole card is now a button
     <div
       onClick={toggleDescription}
-      className="mx-2 mb-4 flex cursor-pointer flex-col rounded-xl border-b-2 border-gray-300 p-4 transition-all duration-300 ease-in-out hover:shadow-lg"
+      className="mx-2 my-4 flex cursor-pointer flex-col rounded-lg bg-gray-200 p-4 shadow-lg"
     >
       {/* Job Title and Company Name */}
       <div className="flex flex-row items-center justify-between">
@@ -35,59 +35,64 @@ const JobSeekerJobPage = ({ jobs }) => {
         <span className="">{payrange[2]}</span>
       </p>
       <p className="ml-3 mt-2 text-sm text-gray-600">
-        Dispatch Location:{" "}
-        <span className="text-orange-600">{jobs.dispatchlocation}</span>
+        <strong>Dispatch Location:</strong> {jobs.dispatchlocation}
       </p>
 
       {/* Expandable Job Description */}
       <div
         ref={contentRef}
-        className={`transition-max-height ml-3 mt-3 overflow-hidden whitespace-pre-wrap text-sm text-gray-700 duration-500 ease-in-out ${
-          isExpanded ? "max-h-[1000px]" : "max-h-5"
+        className={`overflow-hidden transition-all duration-500 ease-in-out ${
+          isExpanded ? "max-h-[2500px]" : "max-h-0"
         }`}
+        style={{ transitionProperty: "max-height" }}
       >
         <div
           className="prose prose-sm whitespace-pre-line break-words pb-4 text-sm prose-h1:pb-1 prose-h1:pt-2 prose-h1:text-base prose-h1:font-semibold prose-p:my-0 prose-p:py-0 prose-p:text-sm prose-li:my-0 prose-li:py-0 md:ml-6"
           dangerouslySetInnerHTML={{ __html: jobs.description }}
         />
-        {/* {jobs.description} */}
         {/* Additional Information */}
-        {isExpanded && (
-          <div className="ml-3 mt-4">
-            <p className="mb-2 text-lg font-semibold text-orange-600">
-              Additional Information:
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Delivery Locations:</strong> {jobs.deliverylocation}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Experience Required:</strong> {jobs.experiencerequired}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Operating Days:</strong>{" "}
-              {jobs.workdays?.join(", ") || "N/A"}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Company Benefits:</strong>{" "}
-              {jobs.benefits?.join(", ") || "N/A"}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Position Type:</strong> {jobs.jobformat}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>US Work Authorization:</strong> {jobs.workauthorization}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Drug Test:</strong> {jobs.drugtest}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>Background Check:</strong> {jobs.backgroundcheck}
-            </p>
-            <p className="mt-1 text-sm text-gray-600">
-              <strong>DOT Medical Card:</strong> {jobs.dotcard}
-            </p>
-          </div>
-        )}
+        <div className="ml-3 mt-4">
+          <p className="mb-2 text-lg font-semibold text-orange-600">
+            Additional Information:
+          </p>
+          {jobs.additionalpay &&
+            jobs.additionalpay[0] &&
+            jobs.additionalpay[1] && (
+              <p className="mt-1 text-sm text-gray-600">
+                <strong>Additional Pay:</strong> ${jobs.additionalpay[0]} per{" "}
+                {jobs.additionalpay[1]}
+              </p>
+            )}
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Delivery Locations:</strong> {jobs.deliverylocation}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Experience Required:</strong> {jobs.experiencerequired}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Operating Days:</strong>{" "}
+            {jobs.workdays?.join(", ") || "N/A"}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Company Benefits:</strong>{" "}
+            {jobs.benefits?.join(", ") || "N/A"}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Position Type:</strong> {jobs.jobformat}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>US Work Authorization:</strong> {jobs.workauthorization}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Drug Test:</strong> {jobs.drugtest}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>Background Check:</strong> {jobs.backgroundcheck}
+          </p>
+          <p className="mt-1 text-sm text-gray-600">
+            <strong>DOT Medical Card:</strong> {jobs.dotcard}
+          </p>
+        </div>
       </div>
 
       {/* Dynamic "Read More" or "Read Less" text */}
